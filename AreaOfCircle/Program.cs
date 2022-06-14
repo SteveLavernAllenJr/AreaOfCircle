d0e8966223caaf9526a94666b9e5508e0b450b45
Steve Allen <stevelavernallenjr@gmail.com>
2022-06-14 18:27:19 -0500

    initial commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double areaOfCircle = (Math.PI * radius * radius);
+            Console.WriteLine($"The Area of a Circle with radius of {radius} is {areaOfCircle}");
+        }
+    }
+}
